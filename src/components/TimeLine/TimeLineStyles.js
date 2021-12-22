d8dfb60f8f2d5c3e9a9858271e01c259de603a07
mohd-sahill <mohammedsahil692@gmail.com>
2021-12-22 14:26:42 +0530

    changed timeline

--- src/components/TimeLine/TimeLineStyles.js
+++ src/components/TimeLine/TimeLineStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 
 export const CarouselContainer = styled.ul`
   max-width: 1040px;
-  background: #000000;
+  background: #0F1624;
   padding: 0rem;
   list-style:none;
   display: flex;
@@ -40,7 +40,7 @@ export const CarouselMobileScrollNode = styled.div`
 `
 
 export const CarouselItem = styled.div`
-  background: #000000;
+  background: #0F1624;
   border-radius: 3px;
   max-width: 196px;
 
