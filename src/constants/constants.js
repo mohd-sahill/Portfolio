60e640b51b292d0385a42e20dd5abfe5a786f49c
mohd-sahill <mohammedsahil692@gmail.com>
2022-01-01 14:29:36 +0530

    updated projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -15,7 +15,16 @@ export const projects = [
     tags: ['React', 'CoinGeckoAPI', 'Netlify'],
     source: 'https://github.com/mohd-sahill/crypto-project',
     visit: 'https://my-crypto-guide-sahil.netlify.app/',
-    id: 0,
+    id: 1,
+  },
+  {  
+    title: 'responsive microsoft web site clone',
+    description: 'Its a clone of microsofts official website worked with grids and flexbox to achieve this layout',
+    image: '/images/microsoft.png',
+    tags: ['HTML', 'CSS', 'JAVASCRIPT'],
+    source: 'https://github.com/mohd-sahill/microsoft-clone',
+    visit: 'https://sahils-microsoft-clone.netlify.app/',
+    id: 2,
   },
 ];
 
